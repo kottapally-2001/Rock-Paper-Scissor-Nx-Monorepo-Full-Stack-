1fc5928f34a3bec477309d4b6b78b265d079f379
kottapally-2001 <saicharan.kottapally@gmail.com>
2025-12-10 14:28:28 +0530

    Vercel

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://rock-paper-scissor-nx-monorepo-full.vercel.app',
+    ],
     credentials: true,
   });
-  await app.listen(3000);
-  console.log('Backend running on http://localhost:3000');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Backend running on port ${port}`);
 }
 bootstrap();
